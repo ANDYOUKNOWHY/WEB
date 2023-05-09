7152caed885ab3c7f1f1c15ae151683ef6324bbb
ANDYOUKNOWHY <tucnaaak1001@gmail.com>
2023-05-09 13:00:04 +0200

    init

--- pROJEKT/calorie.js
+++ pROJEKT/calorie.js
@@ -1,8 +1,23 @@
 let a = "kcal";
 function myFunction() {
-    var y = document.getElementById("txt1").value;
-    var z = document.getElementById("txt2").value;
-    var w = document.getElementById("txt3").value;
-    var x = 655 +(13,7 * y) +(5 * z) -(6,8 * w);
-    document.getElementById("vysledek").innerHTML = x + a;
+  var vaha = document.getElementById("vaha").value;
+  var vyska = document.getElementById("vyska").value;
+  var vek = document.getElementById("vek").value;
+
+  if (document.getElementsByName("gender")[0].value == "zena") {
+    calories = 655.09 + 9.56 * vaha + 1.84 * vyska - 4.67 * vek;
+  } else {
+    calories = 66.47 + 13.75 * vaha + 5 * vyska - 6.75 * vek;
   }
+
+  if (document.getElementsByName("cil")[0].value == "zhub") {
+    calories *= 0.87;
+  } else if (document.getElementsByName("cil")[0].value == "zust") {
+    calories = calories;
+  } else {
+    calories *= 1.15;
+  }
+
+  calories = Math.trunc(calories);
+  document.getElementById("vysledek").innerHTML = calories + a;
+}
