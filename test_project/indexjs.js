7a46e87d6600279f80ec4fd938684bc7e58cb471
ANDYOUKNOWHY <tucnaaak1001@gmail.com>
2023-05-15 14:54:47 +0200

    něco s  js

--- test_project/indexjs.js
+++ test_project/indexjs.js
@@ -1,8 +1,7 @@
 function zapsat() {
-  // Získání obsahu inputu
   var email = document.getElementById("email").value;
-  // Uložení emailové adresy do localStorage
+
   localStorage.setItem("email", email);
-  // Vypsání úspěšného zápisu do konzole
+
   console.log("Úspěšně zapsáno do localStorage.");
 }
